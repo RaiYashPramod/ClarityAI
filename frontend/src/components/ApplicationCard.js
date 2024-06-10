b0066831ac1946912a7834833876dd8c35861d2b
RaiYashPramod <raiyashpramod16@gmail.com>
2024-06-10 16:49:07 +0530

    minor updates

--- frontend/src/components/ApplicationCard.js
+++ frontend/src/components/ApplicationCard.js
@@ -37,7 +37,7 @@ export const HoverEffect = ({ items, className }) => {
           <AnimatePresence>
             {hoveredIndex === idx && (
               <motion.span
-                className="absolute inset-0 h-full w-full bg-indigo-500/30 block  rounded-3xl"
+                className="absolute inset-0 h-full w-full bg-indigo-500/30 block  rounded-xl"
                 layoutId="hoverBackground"
                 initial={{ opacity: 0 }}
                 animate={{
